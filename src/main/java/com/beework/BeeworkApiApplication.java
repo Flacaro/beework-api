198fdad37842242a34b9c8dea9e79c517ad39126
Flacaro <flavianacaroselli@gmail.com>
2021-05-24 19:10:13 +0200

    Project created

---
+++ src/main/java/com/beework/BeeworkApiApplication.java
@@ -0,0 +1,13 @@
+package com.beework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeworkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeworkApiApplication.class, args);
+    }
+
+}
