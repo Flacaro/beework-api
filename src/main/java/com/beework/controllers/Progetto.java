04019d88fac3f29e7acfed81fa8b60e724ef02bb
Flacaro <flavianacaroselli@gmail.com>
2021-06-09 21:18:04 +0200

    Prime prove per il mapping

---
+++ src/main/java/com/beework/controllers/Progetto.java
@@ -0,0 +1,38 @@
+package com.beework.controllers;
+
+public class Progetto {
+    public String id;
+    public String nome;
+    public String descrizione;
+
+    public Progetto(String id, String nome, String descrizione) {
+        this.id = id;
+        this.nome = nome;
+        this.descrizione = descrizione;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+}
+
