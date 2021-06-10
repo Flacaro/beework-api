dcbdcd51815438605e1c81be1fa50fb0c5cc1c98
Flacaro <flavianacaroselli@gmail.com>
2021-06-10 11:55:10 +0200

    Aggiunti model

--- src/main/java/com/beework/models/Utente.java
+++ src/main/java/com/beework/models/Utente.java
@@ -1,4 +1,69 @@
 package com.beework.models;
 
 public class Utente {
+    private String username;
+    private String password;
+    private String nome;
+    private String cognome;
+    private String email;
+    private String bio;
+
+
+    public Utente(String username, String password, String nome, String cognome, String email, String bio) {
+        this.username = username;
+        this.password = password;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.bio = bio;
+
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
 }
