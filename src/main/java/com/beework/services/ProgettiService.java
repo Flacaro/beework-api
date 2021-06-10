436cd058f33b991944920c2a99ddb5bcfc778bd3
AngeloC99 <angelocasciani@live.it>
2021-06-10 15:05:47 +0200

    Aggiunta gestione Utente

--- src/main/java/com/beework/services/ProgettiService.java
+++ src/main/java/com/beework/services/ProgettiService.java
@@ -11,33 +11,35 @@ import java.util.List;
 //Business service
 @Service
 public class ProgettiService {
-List<Progetto> progetti = new ArrayList<> (Arrays.asList(
-        new Progetto("1","Studio delfini", "Devono essere aiutati"),
-                new Progetto("2","Ricerca sul citoplasma","cellule")
-        ));
-public List<Progetto> getProgetti() {
+    List<Progetto> progetti = new ArrayList<> (Arrays.asList(
+            new Progetto("1","Studio delfini", "Devono essere aiutati"),
+            new Progetto("2","Ricerca sul citoplasma","cellule")
+    ));
+
+    public List<Progetto> getProgetti() {
     return progetti;
 }
 
-public Progetto getProgetto(String id){
-    return progetti.stream().filter(t -> t.getId().equals(id)).findFirst().get();
-}
+    public Progetto getProgetto(String id){
+        return progetti.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
 
-public void aggiungiProgetto(Progetto progetto){
+    public void aggiungiProgetto(Progetto progetto){
     progetti.add(progetto);
 }
-public void eliminaProgetto(String id){
+
+    public void eliminaProgetto(String id){
     progetti.stream().filter(t -> t.getId().equals(id));
 }
-public void modificaProgetto(String id, Progetto progetto) {
+
+    public void modificaProgetto(String id, Progetto progetto) {
     //loop per trovare quel progetto specifico
-    for(int i = 0; i < progetti.size(); i++){
-        Progetto t = progetti.get(i);
-        if (t.getId().equals(id)) {
-            progetti.set(i, progetto);
-            return;
+        for(int i = 0; i < progetti.size(); i++){
+            Progetto t = progetti.get(i);
+            if (t.getId().equals(id)) {
+                progetti.set(i, progetto);
+                return;
+            }
         }
     }
-}
-
 }
