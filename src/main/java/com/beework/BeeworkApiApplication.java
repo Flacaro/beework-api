b25a31f39a091b881a6567c0d763734df6628da4
AngeloC99 <angelocasciani@live.it>
2021-06-12 14:29:56 +0200

    Modifiche gestione Commento

--- src/main/java/com/beework/BeeworkApiApplication.java
+++ src/main/java/com/beework/BeeworkApiApplication.java
@@ -1,7 +1,11 @@
 package com.beework;
 
+import com.beework.models.Progetto;
+import com.beework.repositories.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeeworkApiApplication {
@@ -10,4 +14,10 @@ public class BeeworkApiApplication {
         SpringApplication.run(BeeworkApiApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner loadData(UtenteRepository utenteRepository, TaskRepository taskRepository, ProgettiRepository progettiRepository, NotificaRepository notificaRepository, CommentoRepository commentoRepository) {
+        return (args) -> {
+            Progetto p1 = new Progetto();
+        };
+    }
 }
